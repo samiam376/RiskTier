b108d1c1d7713556ad6478d3a9033d96dd245938
samiam376 <ssheridan211@gmail.com>
2022-06-07 18:45:40 -0700

    prettier

--- client/riskfe/components/RiskTable.tsx
+++ client/riskfe/components/RiskTable.tsx
@@ -1,57 +1,63 @@
 import {
-    Table,
-    Thead,
-    Tbody,
-    Tfoot,
-    Tr,
-    Th,
-    Td,
-    TableCaption,
-    TableContainer,
-  } from '@chakra-ui/react'
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from "@chakra-ui/react";
 
 export interface RiskTableProps {
-    rejection: string;
-    riskTier: number | null;
-    iSORiskTier: number | null;
-    stateRiskTier: number | null;
-    techUsageModifier: number | null;
-    referred: string | null;
+  rejection: string;
+  riskTier: number | null;
+  iSORiskTier: number | null;
+  stateRiskTier: number | null;
+  techUsageModifier: number | null;
+  referred: string | null;
 }
 
-export const RiskTable:  React.FC<RiskTableProps> = ( {rejection, riskTier, iSORiskTier, stateRiskTier, techUsageModifier, referred} ) => {
-    return (
-        <TableContainer>
-        <Table variant='simple'>
-          <TableCaption placement='top'>Risk Output</TableCaption>
-          <Tbody>
-            <Tr>
-              <Td fontWeight='extrabold'>Risk Tier</Td>
-              <Td isNumeric>{riskTier}</Td>
-            </Tr>
-            <Tr>
-              <Td fontWeight='extrabold'>ISO Risk Tier</Td>
-              <Td isNumeric>{iSORiskTier}</Td>
-            </Tr>
-            <Tr>
-              <Td fontWeight='extrabold'>State Risk Tier</Td>
-              <Td isNumeric>{stateRiskTier}</Td>
-            </Tr>
-            <Tr>
-              <Td fontWeight='extrabold'>Tech Usage Modifier</Td>
-              <Td isNumeric>{techUsageModifier}</Td>
-            </Tr>
-            <Tr>
-              <Td fontWeight='extrabold'>Rejected</Td>
-              <Td >{rejection}</Td>
-            </Tr>
-            <Tr>
-              <Td fontWeight='extrabold'>Referred</Td>
-              <Td >{referred}</Td>
-            </Tr>
-          </Tbody>
-        </Table>
-      </TableContainer>
-    )
-}
-
+export const RiskTable: React.FC<RiskTableProps> = ({
+  rejection,
+  riskTier,
+  iSORiskTier,
+  stateRiskTier,
+  techUsageModifier,
+  referred,
+}) => {
+  return (
+    <TableContainer>
+      <Table variant="simple">
+        <TableCaption placement="top">Risk Output</TableCaption>
+        <Tbody>
+          <Tr>
+            <Td fontWeight="extrabold">Risk Tier</Td>
+            <Td isNumeric>{riskTier}</Td>
+          </Tr>
+          <Tr>
+            <Td fontWeight="extrabold">ISO Risk Tier</Td>
+            <Td isNumeric>{iSORiskTier}</Td>
+          </Tr>
+          <Tr>
+            <Td fontWeight="extrabold">State Risk Tier</Td>
+            <Td isNumeric>{stateRiskTier}</Td>
+          </Tr>
+          <Tr>
+            <Td fontWeight="extrabold">Tech Usage Modifier</Td>
+            <Td isNumeric>{techUsageModifier}</Td>
+          </Tr>
+          <Tr>
+            <Td fontWeight="extrabold">Rejected</Td>
+            <Td>{rejection}</Td>
+          </Tr>
+          <Tr>
+            <Td fontWeight="extrabold">Referred</Td>
+            <Td>{referred}</Td>
+          </Tr>
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
--- client/riskfe/pages/index.tsx
+++ client/riskfe/pages/index.tsx
@@ -1,5 +1,5 @@
-import { Center, Container, Flex, Spacer, Square } from '@chakra-ui/layout'
-import type { NextPage } from 'next'
+import { Center, Container, Flex, Spacer, Square } from "@chakra-ui/layout";
+import type { NextPage } from "next";
 import {
   Table,
   Thead,
@@ -16,39 +16,39 @@ import {
   FormLabel,
   Divider,
   Button,
-} from '@chakra-ui/react'
-import { RiskTable, RiskTableProps } from '../components/riskTable'
-import React from 'react'
+} from "@chakra-ui/react";
+import { RiskTable, RiskTableProps } from "../components/riskTable";
+import React from "react";
 
 const fakeData: RiskTableProps = {
-  rejection: 'TRUE',
+  rejection: "TRUE",
   riskTier: 10,
   iSORiskTier: 7,
   stateRiskTier: 5,
   techUsageModifier: 0.75,
-  referred: 'TRUE'
-}
+  referred: "TRUE",
+};
 
 const Home: NextPage = () => {
   return (
     <Container>
-      <Flex alignItems='center'>
-      <Stack spacing={2}>
-        <FormLabel htmlFor='state'>State</FormLabel>
-        <Select size='md' borderColor='black' />
-        <FormLabel htmlFor='isocode'>ISO code</FormLabel>
-        <Select size='md' borderColor='black'/>
-        <FormLabel htmlFor='tus'>Technical Usage Grade</FormLabel>
-        <Select size='md' borderColor='black'/>
-        <Button colorScheme='red'>Calculate</Button>
-    </Stack> 
-    <Spacer/>
-    <Divider orientation='vertical'/>
-      <RiskTable {...fakeData}></RiskTable>
-      <Divider orientation='vertical'/> 
-    </Flex>
+      <Flex alignItems="center">
+        <Stack spacing={2}>
+          <FormLabel htmlFor="state">State</FormLabel>
+          <Select size="md" borderColor="black" />
+          <FormLabel htmlFor="isocode">ISO code</FormLabel>
+          <Select size="md" borderColor="black" />
+          <FormLabel htmlFor="tus">Technical Usage Grade</FormLabel>
+          <Select size="md" borderColor="black" />
+          <Button colorScheme="red">Calculate</Button>
+        </Stack>
+        <Spacer />
+        <Divider orientation="vertical" />
+        <RiskTable {...fakeData}></RiskTable>
+        <Divider orientation="vertical" />
+      </Flex>
     </Container>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
